6a650fa1a6db8b3d917d1f941b9c24d3a635d222
unknown <milolfatmi@gmail.com>
2020-06-05 20:29:33 +0100

    Updates

---
+++ precache-manifest.0d00b3f95bab98b7786cef9a4d604265.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f12b5c1f3839c5ce34b6a53686befc47",
+    "url": "/github.io/testReact/index.html"
+  },
+  {
+    "revision": "24fccd9bee03b797a435",
+    "url": "/github.io/testReact/static/css/main.143ee44d.chunk.css"
+  },
+  {
+    "revision": "7214f489ac413ba5f47e",
+    "url": "/github.io/testReact/static/js/2.b73e2fe8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github.io/testReact/static/js/2.b73e2fe8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24fccd9bee03b797a435",
+    "url": "/github.io/testReact/static/js/main.6782199e.chunk.js"
+  },
+  {
+    "revision": "b7c9691c59999669d0a6",
+    "url": "/github.io/testReact/static/js/runtime-main.ee874076.js"
+  }
+]);
